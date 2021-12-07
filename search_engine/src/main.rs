bfaa86ad89abf641989ab49608f05925d0f600a8
hdzhao <hdzhao214@gmail.com>
2021-12-07 12:06:55 +0800

    zhd 12/7: modified json handling in search_engine

--- search_engine/src/main.rs
+++ search_engine/src/main.rs
@@ -1,18 +1,15 @@
 use search_base::*;
 use std::io::Read;
-use serde_json::{Result, Value};
+use serde_json::{Value};
 use tantivy::{
-    collector::TopDocs,
-    query::QueryParser,
-    schema::*,
-    Index,
-    ReloadPolicy,
     doc
 };
 
 fn main() {
     
     let path = "../index_dir";
+    // base_result.0 is the index
+    // base_result.1 is the struct ProjectDocument handling the fields
     let base_result = index(path).unwrap();
 
     // extract index writer
@@ -22,14 +19,17 @@ fn main() {
     let mut file = std::fs::File::open("../test_files/JianShu.txt").unwrap();
     let mut contents = String::new();
     file.read_to_string(&mut contents).unwrap();
+    let mut lines = contents.lines();
     
-    //parse json and write documents into index
-    let mut json_start_idx = 0;
-    let len = contents.as_bytes().len();
+    // parse json and write documents into index
     loop {
-        let next_json_string_idx = find_next_json_string_idx(&contents, json_start_idx);
-        let mut json_string = &contents[json_start_idx..next_json_string_idx-1];
-        let json_object: Value = serde_json::from_str(json_string).unwrap();
+        let line = match lines.next() {
+            Some(i) => i,
+            None => {
+                break;
+            }
+        };
+        let json_object: Value = serde_json::from_str(line).unwrap();
         index_writer.add_document(doc!(
             base_result.1.title => json_object["title"],
             base_result.1.body => json_object["content"],
@@ -38,10 +38,6 @@ fn main() {
             base_result.1.time => json_object["date"],
             base_result.1.likes => json_object["views"]
         ));
-        json_start_idx = next_json_string_idx;
-        if (next_json_string_idx - 1) == len {
-            break;
-        }
     }
 
     // commit index writer
@@ -53,39 +49,39 @@ fn main() {
     }
 }
 
-fn find_next_json_string_idx(s: &str, start_idx: usize) -> usize {
-    let bytes = (&s[start_idx+1..]).as_bytes();
-    let mut title_match = false;
-    let mut title = [false; 9];
+// fn find_next_json_string_idx(s: &str, start_idx: usize) -> usize {
+//     let bytes = (&s[start_idx+1..]).as_bytes();
+//     let mut title_match = false;
+//     let mut title = [false; 9];
 
-    for (i, &item) in bytes.iter().enumerate() {
-        if item == b'{' {
-            title[0] = true;
-            for j in 1..9 {
-                title[j] = false;
-            }
-            title_match = true;
-        } else if title_match == true {
-            let mut now_index = 0; 
-            for j in 1..9 {
-                if title[j] != true {
-                    now_index = j;
-                    break;
-                }
-            }
-            if (now_index == 1 && item == b'"') || (now_index == 2 && item == b't') || (now_index == 3 && item == b'i') || (now_index == 4 && item == b't') || (now_index == 5 && item == b'l') || (now_index == 6 && item == b'e') || (now_index == 7 && item == b'"') {
-                title[now_index] = true;
-            } else if now_index == 8 && item == b':'{
-                // 返回的位置在'{"title":'的'{'这个位置
-                return (start_idx + 1) + (i - 8);
-            } else {
-                title_match = false;
-                for j in 0..9 {
-                    title[j] = false;
-                }
-            }
-        }
-    }
+//     for (i, &item) in bytes.iter().enumerate() {
+//         if item == b'{' {
+//             title[0] = true;
+//             for j in 1..9 {
+//                 title[j] = false;
+//             }
+//             title_match = true;
+//         } else if title_match == true {
+//             let mut now_index = 0; 
+//             for j in 1..9 {
+//                 if title[j] != true {
+//                     now_index = j;
+//                     break;
+//                 }
+//             }
+//             if (now_index == 1 && item == b'"') || (now_index == 2 && item == b't') || (now_index == 3 && item == b'i') || (now_index == 4 && item == b't') || (now_index == 5 && item == b'l') || (now_index == 6 && item == b'e') || (now_index == 7 && item == b'"') {
+//                 title[now_index] = true;
+//             } else if now_index == 8 && item == b':'{
+//                 // 返回的位置在'{"title":'的'{'这个位置
+//                 return (start_idx + 1) + (i - 8);
+//             } else {
+//                 title_match = false;
+//                 for j in 0..9 {
+//                     title[j] = false;
+//                 }
+//             }
+//         }
+//     }
 
-    return (start_idx + 1) + (bytes.len() + 1);
-}
+//     return (start_idx + 1) + (bytes.len() + 1);
+// }
