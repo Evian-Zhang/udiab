432acbf87cde250c0b81bd9825fd1ca2208ec928
Evian-Zhang <evianzhang1999@163.com>
2021-12-07 14:52:31 +0800

    Fix indexer bugs due to the wrong syntax of data file

--- search_engine/src/main.rs
+++ search_engine/src/main.rs
@@ -1,13 +1,38 @@
+use chrono::{DateTime, NaiveDateTime, Utc};
 use search_base::*;
+use serde::Deserialize;
+use serde_json::Value;
+use std::fs;
 use std::io::Read;
-use serde_json::{Value};
-use tantivy::{
-    doc
-};
+use tantivy::doc;
+
+#[derive(Deserialize)]
+struct ArticleInfo {
+    title: String,
+    content: Vec<String>,
+    code: Vec<String>,
+    url: String,
+    date: u64,
+    views: u64,
+}
+
+#[derive(Deserialize)]
+struct IndexerConfig {
+    index_store_directory: String,
+    data_path: String,
+}
 
 fn main() {
-    
-    let path = "index_dir";
+    let config_file_path = "./indexer-config.toml";
+    let config_str = fs::read_to_string(&config_file_path).expect(&format!(
+        "Unable to open config file at {}.",
+        &config_file_path
+    ));
+    let config: IndexerConfig = match toml::from_str(&config_str) {
+        Ok(config) => config,
+        Err(error) => panic!("Config file parse failed: {}", error),
+    };
+    let path = &config.index_store_directory;
     // base_result.0 is the index
     // base_result.1 is the struct ProjectDocument handling the fields
     let base_result = index(path).unwrap();
@@ -16,60 +41,53 @@ fn main() {
     let mut index_writer = base_result.0.writer(50_000_000).unwrap();
 
     // read documents
-    let mut file = std::fs::File::open("test_files\\JianShu.txt").unwrap();
-    let mut buf = vec![];
-    match file.read_to_end(&mut buf) {
-        Ok(i) => {},
-        Err(j) => {
-            panic!("read_to_end error!");
-        }
-    }
-    let contents = String::from_utf8_lossy(&buf);
+    // let mut file = std::fs::File::open("test_files/JianShu.txt").unwrap();
+    // let mut buf = vec![];
+    // match file.read_to_end(&mut buf) {
+    //     Ok(i) => {},
+    //     Err(j) => {
+    //         panic!("read_to_end error!");
+    //     }
+    // }
+    // let contents = String::from_utf8_lossy(&buf);
+    // let contents = file.read_to_string().unwrap();
+    let contents = fs::read_to_string(&config.data_path).unwrap();
     let mut lines = contents.lines();
     // let mut contents = String::new();
     // file.read_to_string(&mut contents).unwrap();
     // let mut lines = contents.lines();
-    
+
     // parse json and write documents into index
-    loop {
-        let line = match lines.next() {
-            Some(i) => i,
-            None => {
-                break;
-            }
+    let mut a = 0;
+    for line in lines {
+        a += 1;
+        if line.trim().len() == 0 {
+            continue;
+        }
+        let json_object: ArticleInfo = if let Ok(json_object) = serde_json::from_str(line.trim()) {
+            json_object
+        } else {
+            println!("{}", a);
+            return;
         };
-        let json_object: Value = serde_json::from_str(line).unwrap();
+        let naive_datetime = NaiveDateTime::from_timestamp(
+            (json_object.date / 1000) as i64,
+            (json_object.date % 1000) as u32,
+        );
+        let date = DateTime::from_utc(naive_datetime, Utc);
         index_writer.add_document(doc!(
-            base_result.1.title => match json_object["title"].as_str() {
-                Some(s) => s,
-                _ => ""
-            },
-            base_result.1.body => match json_object["content"].as_str() {
-                Some(s) => s,
-                _ => ""
-            },
-            base_result.1.code => match json_object["code"].as_str() {
-                Some(s) => s,
-                _ => ""
-            },
-            base_result.1.url => match json_object["url"].as_str() {
-                Some(s) => s,
-                _ => ""
-            },
-            base_result.1.time => match json_object["date"].as_str() {
-                Some(s) => s,
-                _ => ""
-            },
-            base_result.1.likes => match json_object["views"].as_u64() {
-                Some(s) => s,
-                _ => 0
-            }
+            base_result.1.title => json_object.title,
+            base_result.1.body => json_object.content.concat(),
+            base_result.1.code => json_object.code.concat(),
+            base_result.1.url => json_object.url,
+            base_result.1.time => date,
+            base_result.1.likes => json_object.views,
         ));
     }
 
     // commit index writer
     match index_writer.commit() {
-        Ok(i) => {},
+        Ok(i) => {}
         Err(j) => {
             panic!("commit error!");
         }
@@ -89,7 +107,7 @@ fn main() {
 //             }
 //             title_match = true;
 //         } else if title_match == true {
-//             let mut now_index = 0; 
+//             let mut now_index = 0;
 //             for j in 1..9 {
 //                 if title[j] != true {
 //                     now_index = j;
